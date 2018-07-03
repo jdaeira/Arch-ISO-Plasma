468e142b8eb484ccaccf1626cab41d13af71dac9
jdaeira <jdaeira@gmail.com>
2018-07-02 19:11:53 -0700

    add scripts folder and os-release file

---
+++ Scripts/100-display-manager-and-desktop-v1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+echo "#################################################"
+echo "If it feels like the downloads are too slow"
+echo "Stop the installation with CTRL + C"
+echo "and run the alias - mirror in the terminal"
+echo "#################################################"
+
+echo "Removing xcursor-breeze to avoid conflict with breeze"
+sudo pacman -R xcursor-breeze --noconfirm
+
+sudo pacman -Syyu
+sudo pacman -S sddm sddm-kcm --noconfirm --needed
+sudo pacman -S plasma-meta --noconfirm --needed
+sddm --example-config | sudo tee /etc/sddm.conf
+sudo systemctl enable -f sddm
+
+echo "Remove anything you do not like from the installed applications"
+
+
