7fc80ae9ae369cbd0bac52588dfbd9116d7a6835
jdaeira <jdaeira@gmail.com>
2018-07-05 10:27:43 -0700

    add install sound script

---
+++ Scripts/110-install-sound-v3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#Sound
+sudo pacman -S pulseaudio --noconfirm --needed
+sudo pacman -S pulseaudio-alsa --noconfirm --needed
+sudo pacman -S pavucontrol  --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
+sudo pacman -S gstreamer --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly --noconfirm --needed
+#sudo pacman -S volumeicon --noconfirm --needed
+#sudo pacman -S playerctl --noconfirm --needed
+
+echo "################################################################"
+echo "#########   sound software software installed   ################"
+echo "################################################################"
