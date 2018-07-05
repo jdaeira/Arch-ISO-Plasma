366ff64dfaf6cdede10a084dfd512b749cc0a74a
jdaeira <jdaeira@gmail.com>
2018-07-05 15:55:19 -0700

    add Powerline folder

---
+++ Powerline/install-powerline.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# install pip 
+# install powerline
+# need powerline-fonts from pacman
+# create config file
+# add lines to .bashrc file: nano ~/.bashrc
+  function _update_ps1() {
+    PS1=$(powerline-shell $?)
+}
+
+if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
