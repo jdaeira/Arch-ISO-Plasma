ce65ffa5d5c96cf7b8e5e574d7a9d4d71303b008
jdaeira <jdaeira@gmail.com>
2018-08-03 18:54:47 -0700

    modified kernel install

--- Arch Install/000-lts-kernel-install.sh
+++ Arch Install/000-lts-kernel-install.sh
@@ -7,9 +7,9 @@ set -e
 
 
 
-pacman -S linux-headers --noconfirm --needed
-pacman -S linux-lts --noconfirm --needed
-pacman -S linux-lts-headers --noconfirm --needed
+sudo pacman -S linux-headers --noconfirm --needed
+sudo pacman -S linux-lts --noconfirm --needed
+sudo pacman -S linux-lts-headers --noconfirm --needed
 
 
 echo "################################################################"
