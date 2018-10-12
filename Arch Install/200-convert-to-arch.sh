3e66ac8acfd508e7fa286784607f99d6ffd3a299
John da Eira <jdaeira@gmail.com>
2018-10-12 10:33:25 -0700

    Made a change to the 200 convert to arch script

--- Arch Install/200-convert-to-arch.sh
+++ Arch Install/200-convert-to-arch.sh
@@ -15,7 +15,7 @@ sudo cat os-release > /etc/os-release
 sudo cat lsb-release > /etc/lsb-release
 sudo cat grub > /etc/default/grub
 sudo cat pacman.conf > /etc/pacman.conf
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 
 echo "################################################################"
