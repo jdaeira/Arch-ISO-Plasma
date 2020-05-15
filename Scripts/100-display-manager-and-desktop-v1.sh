3914d01805c80b2c147e9ddd0066be40adf0c385
John da Eira <jdaeira@gmail.com>
2020-05-15 16:15:41 -0700

    Update 100-display-manager-and-desktop-v1.sh

--- Scripts/100-display-manager-and-desktop-v1.sh
+++ Scripts/100-display-manager-and-desktop-v1.sh
@@ -19,7 +19,7 @@ sudo pacman -S konsole --noconfirm --needed
 sudo pacman -S sddm sddm-kcm --noconfirm --needed
 sudo pacman -S plasma --noconfirm --needed
 #sudo pacman -S plasma-dekstop --noconfirm --needed   ## More minimal installation
-sddm --example-config | sudo tee /etc/sddm.conf
+#sddm --example-config | sudo tee /etc/sddm.conf
 sudo systemctl enable -f sddm
 
 echo "Remove anything you do not like from the installed applications"
